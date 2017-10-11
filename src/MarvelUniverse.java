cd35c26e3311671d165b52e0ce4a7ff1ed4851b0
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-10-10 21:54:44 -0600

    assignment completed

--- src/MarvelUniverse.java
+++ src/MarvelUniverse.java
@@ -11,17 +11,26 @@ public class MarvelUniverse {
         try {
 
             //PART 1 and 2
-
-            // The first 2 parts require number of heroes and comics, that can be extracted from nodes.csv
+            //The first 2 parts require number of heroes and comics, that can be extracted from nodes.csv
 
             // Reading in the file
-            File marvel_file = new File("nodes.csv");
+
+            File marvel_file = new File("nodes.csv");  // file created
             FileReader file_reader = new FileReader(marvel_file);
+
+            /*
+            * FileReader is the convenience class for reading character files
+            * Meant for reading streams of characters
+            */
             BufferedReader b = new BufferedReader(file_reader);
+            /* BufferredReader reads text from a character-input stream,
+               buffering characters so as to provide for the efficient
+               reading of characters, arrays, and lines.
+             */
 
             int count=0, com_count=0, hero_count=0;
 
-            for (String line = b.readLine(); line != null; line = b.readLine()) {
+            for (String line = b.readLine(); line != null; line = b.readLine()) {      // go through each line
                 count++;
                 if (count==1)       // To ignore 1st line of csv file
                     continue;
@@ -37,18 +46,55 @@ public class MarvelUniverse {
 
             //PART 3 and 4
 
-            //The 3rd and 4th part require total count of all participations of heroes in every comic
+            //The 3rd and 4th part require total count of all participation of heroes in every comic
             //which we can get from edges.csv
 
+            // Reading in the file
+            marvel_file = new File("edges.csv");
+            file_reader = new FileReader(marvel_file);
+            b = new BufferedReader(file_reader);
+            int file_count=0;
+            count = 0;
 
-            
+            for (String line = b.readLine(); line != null; line = b.readLine()) {
+                count++;
+                if (count==1)
+                    continue;
 
+                file_count++;
+            }
+
+            // Total sum of heroes in all comics / number of comics = mean heroes / comic
+            // Total sum of heroes in all comics / number of heroes = mean comics / heroes
+
+            System.out.println("Mean Heroes per comic : " + file_count/com_count);
+            System.out.println("Mean Comics per hero : " + file_count/hero_count);
+
+            //PART 4
+
+            marvel_file = new File("hero-network.csv");
+            file_reader = new FileReader(marvel_file);
+            b = new BufferedReader(file_reader);
+            file_count=0;
+            count = 0;
+
+            for (String line = b.readLine(); line != null; line = b.readLine()) {
+                count++;
+                if (count==1)
+                    continue;
+
+                file_count++;
+            }
+
+            // Total sum of all partners to every hero / number of heroes = mean partner / hero
+
+            System.out.println("Mean Partners per hero : " + file_count/hero_count);
 
         } catch (IOException e) { System.out.println("File not found"); }     // file not found
         //catch (IOException e) { e.printStackTrace();     // could use this instead to print out the error
 
-
     }
 }
+
 
 
